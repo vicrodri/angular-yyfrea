1a90fb346e39c8ee92f2510a1549c14bd6e836e1
vicrodri <vicrodri@protonmail.com>
2019-11-22 14:17:35 +0100

    Getting Started - añadido formulario

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 import { CartService } from '../cart.service';
 
@@ -10,11 +11,26 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   items;
+  checkoutForm;
 
-  constructor(private cartService: CartService) { }
+  constructor(
+    private cartService: CartService, 
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
   }
 
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
 }
